cb59bff7c37aedb914bb0f4fa0d37a52d9370677
tymaestro <tymaestro22@gmail.com>
2022-05-10 13:57:38 +0000

    make initial migrations

--- posts/models.py
+++ posts/models.py
@@ -15,7 +15,7 @@ class Activity(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = [-pub_date]
+        ordering = ['-pub_date']
     
     def __str__(self):
         return self.title
