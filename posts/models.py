646dc199a0113b0ab391bd322452a04f5ca67078
tymaestro <tymaestro22@gmail.com>
2022-05-10 13:51:56 +0000

    add first model and basic structure readme file

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Activity(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    athlete = models.ForeignKey(User, on_delete=models.CASCADE, related_name="activity_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = [-pub_date]
+    
+    def __str__(self):
+        return self.title
