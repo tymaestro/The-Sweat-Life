2f42a22f03b21792fb7c5e49b5ad0943a16f65f8
tymaestro <tymaestro22@gmail.com>
2022-05-10 14:18:19 +0000

    add basic admin functionality

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Activity
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Activity)
+class ActivityAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
