2d1373faae21e8f42308b6e49b33d9e62023b010
tymaestro <tymaestro22@gmail.com>
2022-05-16 17:00:59 +0000

    create basic form on create_activity page

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.ActivityList.as_view(), name='activity_list'),
     path('activities/<int:pk>', views.ActivityView.as_view(), name='activity_detail'),
+    path('create_activity/', views.CreateActivity.as_view(), name='create_activity'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -13,3 +13,9 @@ class ActivityList(generic.ListView):
 class ActivityView(generic.DetailView):
     model = Activity
     template_name = "activity_detail.html"
+
+
+class CreateActivity(generic.CreateView):
+    model = Activity
+    template_name = "create_activity.html"
+    fields = '__all__'
